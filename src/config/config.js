356604c25657c7e2c33e7b75ee5063ed1a667b80
just-codingbaby <gocks5393@naver.com>
2025-01-02 14:23:26 +0900

    feat: 테이블 생성

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const config = {
-  port: process.env.PORT || 8000,
-  dbUrl: process.env.DB_URL,
-};
+// export const config = {
+//   port: process.env.PORT || 8000,
+//   dbUrl: process.env.DB_URL,
+// };
