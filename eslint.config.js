6e18d20d58b4de15f89dbb3e0ce508d29824a508
KJY <139238536+dani784601@users.noreply.github.com>
2025-01-03 11:47:15 +0900

    chore: update ESLint configuration and package scripts; add prettier integration and linting commands

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,19 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
+
 export default [
+  js.configs.recommended,
   {
     languageOptions: {
       globals: globals.browser,
     },
-    extends: ['plugin:js/recommended', 'prettier'],
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+    },
   },
-  pluginJs.configs.recommended,
+  eslintConfigPrettier,
 ];
