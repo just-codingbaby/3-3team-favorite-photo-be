2dcb906aba60c7c7af6d176562ff1f781042a2df
just-codingbaby <gocks5393@naver.com>
2025-01-06 13:28:07 +0900

    feat: CORS 설정

--- src/app.js
+++ src/app.js
@@ -7,7 +7,10 @@ const PORT = config.port;
 const BASE_URL = config.dbUrl;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:8000',
+  credentials: true
+}));
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
