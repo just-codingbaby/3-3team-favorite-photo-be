bd5d023554166c6f60f408fcd7483229b6a85465
Aventurine91 <mandely01@gmail.com>
2025-01-08 16:25:28 +0900

    이메일존재체크 미들웨어 추가

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,12 +2,12 @@ import express from 'express';
 import { signUp } from '../controllers/authController.js';
 import { checkDuplicateUser } from '../middlewares/userMiddleware.js';
 import { login } from '../controllers/authController.js';
+import { checkEmailExists } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/signUp', checkDuplicateUser, signUp);
 
-//로그인
-router.post('/login', login);
+router.post('/login', checkEmailExists, login);
 
 export default router;
