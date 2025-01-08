21c1f76d70023bc214145bed169d8b7fe5dce6cd
Aventurine91 <mandely01@gmail.com>
2025-01-08 16:17:53 +0900

    로그인 포스트맨 테스트완료

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { signUp } from '../controllers/authController.js';
 import { checkDuplicateUser } from '../middlewares/userMiddleware.js';
+import { login } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/signUp', checkDuplicateUser, signUp);
+
+//로그인
+router.post('/login', login);
 
 export default router;
