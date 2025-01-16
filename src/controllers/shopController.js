8f7e938003ae389a6ae778b5221d2714ab44d7bf
KJY <139238536+dani784601@users.noreply.github.com>
2025-01-16 10:17:56 +0900

    feat: 카드조회api 함수명 변경(getAllCards->getCardList)

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -5,9 +5,16 @@ const shopController = express.Router();
 
 shopController.get('/', async (req, res) => {
   // #swagger.tags = ['Shop']
-  const { q, grade, genre } = req.query;
-  const cards = await shopService.getAllCards();
-  return res.json(cards);
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 4;
+  const skip = (page - 1) * limit;
+
+  try {
+    const cards = await shopService.getCardList(skip, limit);
+    res.json(cards);
+  } catch (e) {
+    res.status(500).json({ message: 'Error fetching cards', e });
+  }
 });
 
 shopController.post('/', async (req, res) => {
