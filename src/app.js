c9543cb652b94ad3917b7a73f8a5a4f4261fb7b6
KJY <139238536+dani784601@users.noreply.github.com>
2025-01-17 15:47:34 +0900

    feat: Add morgan for logging and update dependencies.

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
-import { readFileSync, existsSync, mkdirSync } from 'fs';
+import { existsSync, mkdirSync, readFileSync } from 'fs';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { config } from '#config/config.js';
 import apiRoutes from '#routes/routes.js';
-// import router from './routes/upload.routes.js'; // 업로드 라우터
-import createCardRouter from './routes/createcard.routes.js'; // 통합 라우터
-
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
+import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
+// import router from './routes/upload.routes.js'; // 업로드 라우터
+import createCardRouter from './routes/createcard.routes.js'; // 통합 라우터
 
 const app = express();
 const PORT = config.port;
 // const BASE_URL = config.dbUrl;
+
+// 로그 기록
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+} else {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
 app.use(cookieParser());
@@ -34,12 +41,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const uploadPath = join(__dirname, '../uploads'); // 'uploads' 디렉토리 경로 설정
-if (!existsSync(uploadPath)) { // 'uploads' 디렉토리가 존재하지 않으면 생성
+if (!existsSync(uploadPath)) {
+  // 'uploads' 디렉토리가 존재하지 않으면 생성
   mkdirSync(uploadPath);
 }
 app.use('/uploads', express.static(uploadPath)); // 정적 파일 제공
 
-if (process.env.NODE_ENV === 'development') {  
+if (process.env.NODE_ENV === 'development') {
   console.log('Static file directory:', uploadPath);
 }
 
@@ -58,7 +66,6 @@ app.use(`/api/${API_VERSION}`, apiRoutes);
 
 // app.use('/api/v1/images', router);
 app.use('/api/v1/users', createCardRouter);
-
 
 app.listen(PORT, () => {
   if (config.env === 'development') {
