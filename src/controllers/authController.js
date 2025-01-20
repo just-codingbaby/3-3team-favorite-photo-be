d35e4457a71e6e2ed5c88b74b794b11e13945b6e
Kim Doobong <tengal73@naver.com>
2025-01-20 11:47:56 +0900

    Revert "fix:creatorId 에러 fix."
    This reverts commit 348e658ef2abc6fe3f31ba2e0039000696588b66.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -59,6 +59,7 @@ export const login = async (req, res) => {
       { expiresIn: '7d' },
     );
 
+    /*
     // 로컬 환경에서 쿠키 설정
     res.cookie('accessToken', accessToken, {
       httpOnly: true,
@@ -75,8 +76,8 @@ export const login = async (req, res) => {
       path: '/',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 쿠키 유효 기간 7일
     });
+*/
 
-    /*
     // 배포 환경에서의 쿠키 설정
     res.cookie('accessToken', accessToken, {
       httpOnly: true,
@@ -93,7 +94,7 @@ export const login = async (req, res) => {
       path: '/',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 쿠키 유효 기간 7일
     });
-*/
+
     // 응답 반환
     return res.status(200).json({
       message: '로그인 성공',
